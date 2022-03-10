f5749698690aea3f8b378c20eeaa96312d5025f6
Anna Ogneva <as.ogneva@gmail.com>
2022-03-10 12:08:31 +0500

    security added

---
+++ src/main/java/ru/ogneva/springsecurity/SecurityConfig.java
@@ -0,0 +1,30 @@
+package ru.ogneva.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("u1")
+                .password("p1")
+                .authorities("ROLE_USER")
+                .and()
+                .withUser("u2")
+                .password("p2")
+                .authorities("ROLE_USER");
+    }
+
+}
